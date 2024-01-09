c889b298caa742c70162f49e8f0e8aa7474310b9
henokg573 <140075019+henokg573@users.noreply.github.com>
2024-01-09 22:27:43 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-
-def write_file(filename="", text=""):
-    """writes to a utf-8 encoded text file
-    """
-    with open(filename, 'w', encoding='utf-8') as myFile:
-        return myFile.write(text)
